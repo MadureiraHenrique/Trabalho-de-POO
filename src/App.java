1d17e003d45219a498fc7f3912ac72bcf7ba71d9
Robson Santos <santosrobson635@gmail.com>
2025-09-29 23:02:25 -0300

    mudança do java 25 para o java 21, melhorias no codigo

--- src/App.java
+++ src/App.java
@@ -1,46 +1,42 @@
 import java.util.Random;
 
 public class App {
-    static void main() {
+    public static void main(String[] args) {
         int distancia = 20;
-        int carro1 = 0;
-        int carro2 = 0;
-        
+        int distanciaCarro1 = 0;
+        int distanciaCarro2 = 0;
+        int count = 0;
+
         String animacao1 = "";
         String animacao2 = "";
         
         try {
-            for (int i = 0; i < distancia; i++) {
+            while(distanciaCarro1 < distancia || distanciaCarro2 < distancia) {
                 Thread.sleep(1000);
-                IO.println("Corrida " + i);
+                count++;
+                System.out.println("volta " + count);
                 int cache = 0;
-                IO.println("--------------------");
-                if (carro1 == distancia) {
-                    i = distancia;
-                } else if (carro2 == distancia) {
-                    i = distancia;
-                } else {
-                    cache = carro1;
-                    carro1 += aumentarVelocidade();
-                    animacao1 += animacao(carro1 - cache);
-    
-                    cache = carro2;
-                    carro2 += aumentarVelocidade();
-                    animacao2 += animacao(carro2 - cache);
-                    
-                    IO.println(animacao1 + "#");
-                    IO.println(animacao2 + "#");
-                }
-                IO.println("--------------------");
+                System.out.println("--------------------");
+                cache = distanciaCarro1;
+                distanciaCarro1 += aumentarVelocidade();
+                animacao1 += animacao(distanciaCarro1 - cache);
+
+                cache = distanciaCarro2;
+                distanciaCarro2 += aumentarVelocidade();
+                animacao2 += animacao(distanciaCarro2 - cache);
+
+                System.out.println(animacao1 + "#");
+                System.out.println(animacao2 + "#");
+                System.out.println("--------------------");
+                //limparTela();
             }
-            limparTela();
     
-            status(carro1, carro2);
+            status(distanciaCarro1, distanciaCarro2);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
-            IO.println("Thread interrompida durante o sono, o erro é um tal de : " + e.getMessage());
+            System.out.println("Thread interrompida durante o sono, o erro é um tal de : " + e.getMessage());
         }
-
+        System.out.println();
     }
 
     public static int aumentarVelocidade() {
@@ -49,29 +45,27 @@ public class App {
     }
 
     public static void status(int carro1, int carro2) {
-        IO.println("(---> Status <---)");
+        System.out.println("(---> Status <---)");
         if (carro1 > carro2) {
-            IO.println("Carro 1 Ganhou!");
+            System.out.println("Carro 1 Ganhou!");
         } else if (carro2 > carro1) {
-            IO.println("Carro 2 Ganhou!");
+            System.out.println("Carro 2 Ganhou!");
         } else {
-            IO.println("Carro 1 e Carro 2 Empataram!");
+            System.out.println("Carro 1 e Carro 2 Empataram!");
         }
-        IO.println("Velocidade do Carro 1 = " + carro1);
-        IO.println("Velocidade do Carro 2 = " + carro2);
+        System.out.println("distancia do Carro 1 = " + carro1);
+        System.out.println("distancia do Carro 2 = " + carro2);
     }
     
-    public static String animacao(int carro) {
+    public static String animacao(int distanciaCarro) {
         String acrescentar = "";
-        for (int i = 0; i < carro; i++) {
-            acrescentar += " ";
-        }
+        acrescentar = " ".repeat(distanciaCarro);
         return acrescentar;
     }
 
     public static void limparTela() {
         for (int i = 0; i < 50; i++) {
-            IO.println();
+            System.out.println();
         }
     }
 
