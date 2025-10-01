3f4b3090d2bcbef134092c6354a045a286ea2ec3
Henrique Madureira <carloshenriquemadureira.santos@ucsal.edu.br>
2025-10-01 01:48:38 -0300

    Houve uma melhora do design dos carros e dos status, foi melhorado a sintaxe e a complexidade e houve uma limpeza no código!

--- src/App.java
+++ src/App.java
@@ -2,39 +2,48 @@ import java.util.Random;
 
 public class App {
     public static void main(String[] args) {
-        int distancia = 20;
-        int distanciaCarro1 = 0;
-        int distanciaCarro2 = 0;
-        int count = 0;
+        int distanciaCarro1 = 0, distanciaCarro2 = 0, count = 0, distancia = 20;
 
-        String animacao1 = "";
-        String animacao2 = "";
-        
+        String animacao1 = "", animacao2 = "";
+
         try {
+	    	println("Carregando Simulação ...");
+
+	    	Thread.sleep(5000);
+
+			limparTela();
+
+	    	println("Simulação Iniciada!");
+	    	println("-".repeat(distancia));
+            println("#");
+	    	println("#");
+	    	println("-".repeat(distancia));
+
             while(distanciaCarro1 < distancia && distanciaCarro2 < distancia) {
                 Thread.sleep(1000);
                 limparTela();
                 count++;
-                System.out.println("volta " + count);
-                System.out.println(aumentarDistancia());
-                System.out.println("-----------------------");
+
+                println("Volta " + count);
+                println("-".repeat(distancia));
                 distanciaCarro1 += aumentarDistancia();
                 animacao1 = animacao(distanciaCarro1);
 
                 distanciaCarro2 += aumentarDistancia();
                 animacao2 = animacao(distanciaCarro2);
 
-                System.out.println(animacao1 + "#");
-                System.out.println(animacao2 + "#");
-                System.out.println("-----------------------");
+                println(animacao1 + "#");
+                println(animacao2 + "#");
+                println("-".repeat(distancia));
             }
-    
+
             status(distanciaCarro1, distanciaCarro2);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
-            System.out.println("Thread interrompida durante o sono, o erro é um tal de : " + e.getMessage());
+            println("Thread interrompida durante o soninho, o erro é um tal de " + e.getMessage());
         }
-        System.out.println();
+
+        println(" ");
     }
 
     public static int aumentarDistancia() {
@@ -43,27 +52,29 @@ public class App {
     }
 
     public static void status(int carro1, int carro2) {
-        System.out.println("(---> Status <---)");
+        println("\n(---> Status <---)");
         if (carro1 > carro2) {
-            System.out.println("Carro 1 Ganhou!");
+            println("Carro 1 Ganhou!");
         } else if (carro2 > carro1) {
-            System.out.println("Carro 2 Ganhou!");
+            println("Carro 2 Ganhou!");
         } else {
-            System.out.println("Carro 1 e Carro 2 Empataram!");
+            println("Carro 1 e Carro 2 Empataram!");
         }
-        System.out.println("distancia do Carro 1 = " + carro1);
-        System.out.println("distancia do Carro 2 = " + carro2);
+        println("Distancia do Carro 1 = " + carro1);
+        println("Distancia do Carro 2 = " + carro2);
     }
-    
+
     public static String animacao(int distanciaCarro) {
         String animacao = " ".repeat(distanciaCarro);
         return animacao;
     }
 
     public static void limparTela() {
-        for (int i = 0; i < 10; i++) {
-            System.out.println();
-        }
+        println("\n".repeat(10));
+    }
+
+    public static void println(Object o) {
+		System.out.println(o);
     }
 
 }
